c0ef37d9e00f3ffd9a4dcb972a143406e525e7ee
Russell Wallace <russell.wallace@gmail.com>
2024-05-05 19:30:04 +0100

    fix

--- Program.cs
+++ Program.cs
@@ -1,15 +1,16 @@
 ﻿using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.Formats.Jpeg;
+using System.IO;
 
 class Program {
 	static void Main(string[] args) {
-		if (args.Length < 2) {
-			Console.WriteLine("Usage: webp-to-jpg <input.webp> <output.jpg>");
+		if (args.Length < 1) {
+			Console.WriteLine("Usage: webp-to-jpg <input.webp>");
 			return;
 		}
 
 		string inputPath = args[0];
-		string outputPath = args[1];
+		string outputPath = Path.ChangeExtension(inputPath, ".jpg");
 
 		using Image image = Image.Load(inputPath);
 		image.SaveAsJpeg(outputPath, new JpegEncoder { Quality = 100 });
