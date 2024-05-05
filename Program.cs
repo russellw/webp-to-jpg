7d596d8f318cfe4442bee74d24f244b97a06725f
Russell Wallace <russell.wallace@gmail.com>
2024-05-05 19:23:10 +0100

    main

--- Program.cs
+++ Program.cs
@@ -1,7 +1,17 @@
-﻿namespace webp_to_jpg {
-internal class Program {
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats.Jpeg;
+
+class Program {
 	static void Main(string[] args) {
-		Console.WriteLine("Hello, World!");
+		if (args.Length < 2) {
+			Console.WriteLine("Usage: webp-to-jpg <input.webp> <output.jpg>");
+			return;
+		}
+
+		string inputPath = args[0];
+		string outputPath = args[1];
+
+		using Image image = Image.Load(inputPath);
+		image.SaveAsJpeg(outputPath, new JpegEncoder { Quality = 100 });
 	}
-}
 }
