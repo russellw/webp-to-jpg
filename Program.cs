d6e7912091589d86191628e373254b231e34ce39
Russell Wallace <russell.wallace@gmail.com>
2024-05-05 19:17:19 +0100

    initial

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace webp_to_jpg
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
